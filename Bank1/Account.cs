424d43b45a7da3953ae53b1b1efd54edd625f0ad
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-10 16:08:09 -0500

    Add project files.

---
+++ Bank1/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank1 {
+    public class Account {
+        private static int NextAccountNumber { get; set; } = 1;
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public string Description { get; set; }
+
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            Balance += Amount;
+            return true;
+        }
+        public bool Withdraw(decimal Amount) {
+            if(Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            if(Amount > Balance) {
+                throw new Exception("Insufficient funds!");
+            }
+            Balance -= Amount;
+            return true;
+        }
+        public bool Transfer(decimal Amount, Account ToAccount) {
+            try {
+                Withdraw(Amount);
+            } catch(Exception) {
+                throw new Exception("Withdraw failed in Transfer");
+            }
+            ToAccount.Deposit(Amount);
+            return true;
+        }
+
+        public void Debug() {
+            Console.WriteLine($"{AccountNumber}|{Description}|{Balance:c}");
+        }
+        
+        public Account() {
+            AccountNumber = NextAccountNumber;
+            NextAccountNumber++;
+            Description = "New Account";
+            Balance = 0;
+        }
+    }
+}
---
+++ Bank1/Savings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank1 {
+    public class Savings : Account {
+
+        public decimal InterestRate { get; set; } = 0.12m;
+
+        public void CalculateAndPayInterest(int months) {
+            decimal interest = Balance * (InterestRate / 12) * months;
+            Deposit(interest);
+        }
+    }
+}
